3defe6bb12a333e25f372e626b9275ccad24657d
antonio <antonio.dias@siteware.com.br>
2024-05-18 19:50:34 -0300

    adjust basis

--- src/components/photos-carousel/index.tsx
+++ src/components/photos-carousel/index.tsx
@@ -35,17 +35,17 @@ export default function PhotosCarousel() {
         setApi={setApi}
       >
         <CarouselContent className="carousel-content">
-          <CarouselItem className="basis-5/6 md:basis-1/2">
-            <Image className="image" src='/carousel-1.png' width={420} height={900} alt="Foto de Alyce e Antonio juntos" />
+          <CarouselItem className="basis-5/6 md:basis-[45%]">
+            <Image className="image" src='/carousel-1.png' width={900} height={900} alt="Foto de Alyce e Antonio juntos" />
           </CarouselItem>
-          <CarouselItem className="basis-5/6 md:basis-1/2">
-            <Image className="image" src='/carousel-2.png' width={420} height={900} alt="Foto de Alyce e Antonio juntos" />
+          <CarouselItem className="basis-5/6 md:basis-[45%]">
+            <Image className="image" src='/carousel-2.png' width={900} height={900} alt="Foto de Alyce e Antonio juntos" />
           </CarouselItem>
-          <CarouselItem className="basis-5/6 md:basis-1/2">
-            <Image className="image" src='/carousel-3.png' width={420} height={900} alt="Foto de Alyce e Antonio juntos" />
+          <CarouselItem className="basis-5/6 md:basis-[45%]">
+            <Image className="image" src='/carousel-3.png' width={900} height={900} alt="Foto de Alyce e Antonio juntos" />
           </CarouselItem>
-          <CarouselItem className="basis-5/6 md:basis-1/2">
-            <Image className="image" src='/carousel-4.png' width={420} height={900} alt="Foto de Alyce e Antonio juntos" />
+          <CarouselItem className="basis-5/6 md:basis-[45%]">
+            <Image className="image" src='/carousel-4.png' width={900} height={900} alt="Foto de Alyce e Antonio juntos" />
           </CarouselItem>
         </CarouselContent>
         <div className="step-buttons">
