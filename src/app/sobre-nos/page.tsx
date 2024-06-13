f394eebe7b7b82bdabc95e9050306d88602eda63
antonio <antonio.dias@siteware.com.br>
2024-06-13 20:45:07 -0300

    styles fix and finish about us page

--- src/app/sobre-nos/page.tsx
+++ src/app/sobre-nos/page.tsx
@@ -1,46 +1,54 @@
+import Image from 'next/image';
 import './styles.css';
 
 export default function Gifts() {
   return (
-    <main className="about-us-wrapper">
-      <div className="alyce">
-        <h1>Sobre Alyce</h1>
-        <div className="text">
-          <p>
-            &nbsp;&nbsp;Alyce dos Reis Rufino nasceu no dia 27 de janeiro de 2003, uma segunda feira, em Presidente Prudente, SP. Filha de pais missionários, teve a mudança como um fator permanente nos seus dias.
-          </p>
-          <p>
-            &nbsp;&nbsp;Sonhos e arte sempre foram coisas que povoaram seus pensamentos diários e, consequentemente, suas preferências.
-          </p>
-          <p>
-            &nbsp;&nbsp;Além desses fatos, o mais importante: seu amor por Jesus sempre guiou todas as suas mais importantes decisões, desde os seus 12 anos de idade.
-          </p>
-          <p>
-            &nbsp;&nbsp;Dentre várias cidades diferentes, em 2020, se mudou para Lorena, SP, com seus pais, e não tinha ideia das mudanças que estava prestes a viver, mas sabia que viriam.
-          </p>
+    <section className="about-us-wrapper">
+      <div className="header">
+        <div className="texts-side">
+          <div className="alyce">
+            <h1>Sobre Alyce</h1>
+            <div className="text">
+              <p>
+                &nbsp;&nbsp;Alyce dos Reis Rufino nasceu no dia 27 de janeiro de 2003, uma segunda feira, em Presidente Prudente, SP. Filha de pais missionários, teve a mudança como um fator permanente nos seus dias.
+              </p>
+              <p>
+                &nbsp;&nbsp;Sonhos e arte sempre foram coisas que povoaram seus pensamentos diários e, consequentemente, suas preferências.
+              </p>
+              <p>
+                &nbsp;&nbsp;Além desses fatos, o mais importante: seu amor por Jesus sempre guiou todas as suas mais importantes decisões, desde os seus 12 anos de idade.
+              </p>
+              <p>
+                &nbsp;&nbsp;Dentre várias cidades diferentes, em 2020, se mudou para Lorena, SP, com seus pais, e não tinha ideia das mudanças que estava prestes a viver, mas sabia que viriam.
+              </p>
+            </div>
+          </div>
+          <div className="antonio">
+            <h1>Sobre Antonio</h1>
+            <div className="text">
+              <p>
+                &nbsp;&nbsp;Antonio Dias Maia Costa nasceu no dia 28 de julho de 2005, na cidade de São Paulo, SP, sendo o único paulista de sua família, que é originalmente do nordeste.
+              </p>
+              <p>
+                &nbsp;&nbsp;Antonio tem um interesse profundo por estudos bíblicos e busca ser um autêntico discípulo de Jesus desde cedo. Ele é reconhecido por sua criatividade e curiosidade, as quais usa para aprender coisas novas e solucionar problemas. Antonio conseguiu combinar isso com seu interesse por tecnologia, atuando como engenheiro de software desde os 16 anos.
+              </p>
+              <p>
+                &nbsp;&nbsp;Em 2016, ele se mudou para a cidade de Lorena, onde experimentou muitas mudanças, crescimentos e aprendizados. Um dos acontecimentos mais marcantes foi quando conheceu Alyce no final de 2021.
+              </p>
+            </div>
+          </div>
+          <div className="about-us">
+            <h1>Sobre nós</h1>
+            <div className="text">
+              <p>&nbsp;&nbsp;Alyce e Antonio se conheceram por frequentarem juntos a mesma igreja e a amizade, que nasceu de interesses em comum sobre cultura, fé, hobbies e sonhos, evoluiu para namoro, noivado e, muito em breve, casamento! Tudo graças ao Senhor, que permitiu que os caminhos dos dois se entrelaçassem; e você estará lá pra ver o sonho da formação de uma família se tornar realidade ❤️</p>
+              <p>&nbsp;&nbsp;“E lhes darei um mesmo coração, e um só caminho, para que me temam todos os dias, para seu bem, e o bem de seus filhos, depois deles” – Jeremias 32:39</p>
+            </div>
+          </div>
+        </div>
+        <div className="image-side">
+          <Image src='/about-us-photo.png' width={400} height={200} alt='Foto de Antonio e Alyce' />
         </div>
       </div>
-      <div className="antonio">
-        <h1>Sobre Antonio</h1>
-        <div className="text">
-          <p>
-            &nbsp;&nbsp;Antonio Dias Maia Costa nasceu no dia 28 de julho de 2005, na cidade de São Paulo, SP, sendo o único paulista de sua família, que é originalmente do nordeste.
-          </p>
-          <p>
-            &nbsp;&nbsp;Antonio tem um interesse profundo por estudos bíblicos e busca ser um autêntico discípulo de Jesus desde cedo. Ele é reconhecido por sua criatividade e curiosidade, as quais usa para aprender coisas novas e solucionar problemas. Antonio conseguiu combinar isso com seu interesse por tecnologia, atuando como engenheiro de software desde os 16 anos.
-          </p>
-          <p>
-            &nbsp;&nbsp;Em 2016, ele se mudou para a cidade de Lorena, onde experimentou muitas mudanças, crescimentos e aprendizados. Um dos acontecimentos mais marcantes foi quando conheceu Alyce no final de 2021.
-          </p>
-        </div>
-      </div>
-      <div className="about-us">
-        <h1>Sobre nós</h1>
-        <div className="text">
-          <p>Alyce e Antonio se conheceram por frequentarem juntos a mesma igreja e a amizade, que nasceu de interesses em comum sobre cultura, fé, hobbies e sonhos, evoluiu para namoro, noivado e, muito em breve, casamento! Tudo graças ao Senhor, que permitiu que os caminhos dos dois se entrelaçassem; e você estará lá pra ver o sonho da formação de uma família se tornar realidade ❤️</p>
-          <p>“E lhes darei um mesmo coração, e um só caminho, para que me temam todos os dias, para seu bem, e o bem de seus filhos, depois deles” – Jeremias 32:39</p>
-        </div>
-      </div>
-    </main>
+    </section>
   );
 }
